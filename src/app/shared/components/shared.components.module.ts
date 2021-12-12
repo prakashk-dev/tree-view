20536af0b6f4e6e3bd74f9a9a14a00b9b7279b40
prakash <prakash@curvetomorrow.com.au>
2021-12-13 09:13:44 +1100

    Clean up code
    Remove boilerplate code from app component
    Remove primary button component

--- src/app/shared/components/shared.components.module.ts
+++ src/app/shared/components/shared.components.module.ts
@@ -3,10 +3,8 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NodeInputComponent } from './node-input/node-input.component';
 import { NodeNameComponent } from './node-name/node-name.component';
-import { PrimaryButtonComponent } from './primary-button/primary-button.component';
 
 const components = [
-  PrimaryButtonComponent,
   NodeInputComponent,
   NodeNameComponent
 ];
