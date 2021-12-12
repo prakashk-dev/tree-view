0f59eea233b45f317b021d5b359eef7f1afcf29b
prakash <prakash@curvetomorrow.com.au>
2021-12-13 08:56:17 +1100

    Add node input component

--- src/app/shared/components/node-input/node-input.component.ts
+++ src/app/shared/components/node-input/node-input.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 
+export interface NodeInputComponentProps {
+  nodeName: string;
+  nodeIconSrc: string;
+  handleAdd: EventEmitter<string>;
+}
 @Component({
   selector: 'app-node-input',
   templateUrl: './node-input.component.html',
   styleUrls: ['./node-input.component.scss']
 })
-export class NodeInputComponent implements OnInit {
+export class NodeInputComponent implements OnInit, NodeInputComponentProps {
+
+  nodeName = '';
+  @Input() nodeIconSrc: string = '';
+  @Output() handleAdd = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onAdd() {
+    this.handleAdd.emit(this.nodeName);
+  }
+  onRemove() {
+    this.handleAdd.emit('');
+  }
 }
